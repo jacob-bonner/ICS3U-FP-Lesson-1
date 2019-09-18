d21f548ae81b6bc0c94cbc0993dc48023e11f32a
Ubuntu <ubuntu@ip-172-31-27-73.ec2.internal>
2019-09-18 18:41:54 +0000

    Initial commit

---
+++ code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Jacob Bonner
+# Created on: September 2019
+# This is a "Hello, World!" program for the PyBadge
+
+
+def main():
+    # this function prints to the screen and console
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass  # just a place holder
+
+
+if __name__ == "__main__":
+    main()
